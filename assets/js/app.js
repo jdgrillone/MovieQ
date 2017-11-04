3426b8d80623178994ab0a1a9c8dc81050cb271c
Sorenious <30472130+Sorenious@users.noreply.github.com>
2017-11-04 11:46:25 -0600

    Additional search results and Recommend Info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
+	var movie = "";
+	var offset = 0;
 	function movieSearch() {
-
-	};
-      // This function handles events where the search button is clicked
-      $("#movie-search").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
-
-        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=cb3ac66f262794533540ec467d2c75f1&query=" + movie;
+		var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=cb3ac66f262794533540ec467d2c75f1&query=" + movie;
         $(".results").html("");
         // Creating an AJAX call for the specific movie being searched
         $.ajax({
@@ -20,10 +14,10 @@ $(document).ready(function(){
           for (var i = 0; i < 3; i++) {
             // Creating a div to hold the movie
             var movieDiv = $("<div class='movie'>");
-            movieDiv.attr("value", response.results[i].id);
+            movieDiv.attr("value", response.results[i + offset].id);
           
             // Storing the title data
-            var title = response.results[i].title;
+            var title = response.results[i + offset].title;
 
             // // Creating an element to have the rating displayed
             // var pOne = $("<p>").text("Title: " + title);
@@ -32,7 +26,7 @@ $(document).ready(function(){
                     // movieDiv.append(pOne);
 
             // Storing the release year
-            var released = response.results[i].release_date.substring(0, 4);
+            var released = response.results[i + offset].release_date.substring(0, 4);
 
             // Creating an element to hold the release year
             var pTwo = $("<p>").text('"' + title + '" -- ' + released);
@@ -42,18 +36,46 @@ $(document).ready(function(){
           
             // Putting the entire movie in the search results
             $(".results").append(movieDiv);
-            $(".carousel").html("");
-            $(".carousel").attr("class", "carousel");
           }
         });
-      });
+	};
+    // This function handles events where the search button is clicked
+    $("#movie-search").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        movie = $("#movie-input").val().trim();
+        offset = 0;
+        movieSearch();
+        $(".results").html("");
+        $("#movie-input").val("")
+    });
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
+    $(".prev").on("click", function(event) {
+      	event.preventDefault();
+
+      	if (offset >= 3) {
+      		offset -= 3;
+      		movieSearch();
+      	}
+    });
+
+    $(".next").on("click", function(event) {
+      	event.preventDefault();
+
+      	if (offset < 15) {
+      		offset += 3;
+      		movieSearch();
+      	}
+    });
+
+      // displayRecommendations function re-renders the HTML to display the appropriate content
       function displayRecommendations() {
 
         var movieID = $(this).attr("value");
         var queryURL = "https://api.themoviedb.org/3/movie/" + movieID + "/recommendations?api_key=cb3ac66f262794533540ec467d2c75f1&language=en-US&page=1";
 
+            $(".carousel").html("");
+            $(".carousel").attr("class", "carousel");
         // Creating an AJAX call for the specific movie button being clicked
         $.ajax({
           url: queryURL,
@@ -70,6 +92,7 @@ $(document).ready(function(){
           for (var i = 0; i < 10; i++) {
             // Creating a div to hold the movie
             var movieDiv = $("<div class='carousel-item recommend'>");
+            movieDiv.attr("value", response.results[i].id);
 
             // // Storing the rating data
             // var rating = response.Rated;
@@ -113,8 +136,68 @@ $(document).ready(function(){
           }
 
           $(".carousel").carousel();
-          $(".results").html("");
         }
+        });
+
+      }
+
+      // displayRecommendationInfo function re-renders the HTML to display the appropriate content
+      function displayRecommendationInfo() {
+
+        var movieID = $(this).attr("value");
+        var queryURL = "https://api.themoviedb.org/3/movie/" + movieID + "?language=en-US&api_key=cb3ac66f262794533540ec467d2c75f1";
+
+        // Creating an AJAX call for the specific recommendation button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response);          
+
+            // Creating a div to hold the movie
+            var movieDiv = $("<div class='recommend-info'>");
+
+            // Storing the title data
+            var title = response.title;
+
+            // Creating an element to have the rating displayed
+            var pOne = $("<p>").text("Title: " + title);
+
+            // Displaying the title
+            movieDiv.append(pOne);
+
+            // Storing the release year
+            var released = response.release_date;
+
+            // Creating an element to hold the release year
+            var pTwo = $("<p>").text("Released: " + released);
+
+            // Displaying the release year
+            movieDiv.append(pTwo);
+
+            // Storing the plot
+            var plot = response.overview;
+
+            // Creating an element to hold the plot
+            var pThree = $("<p>").text("Plot Summary: " + plot);
+
+            // Appending the plot
+            movieDiv.append(pThree);
+
+            // Retrieving the URL for the image
+            var imgURL = "https://image.tmdb.org/t/p/w185" + response.poster_path;
+
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
+
+            // Appending the image
+            movieDiv.append(image);
+
+            
+
+            // Putting the entire movie above the previous movies
+            $(".s9").append(movieDiv);
+        
         });
 
       }
@@ -122,8 +205,11 @@ $(document).ready(function(){
       // Adding a click event listener to all elements with a class of "movie"
       $(document).on("click", ".movie", displayRecommendations);
 
-      // Adding a click event listener to all elements with a class of "movie"
+      // Adding a click event listener to all elements with a class of "genre"
       $(document).on("click", ".genre", displayRecommendations);
+
+      // Adding a click event listener to all elements with a class of "recommend"
+      $(document).on("click", ".recommend", displayRecommendationInfo);
 
 
 
